be696079b2bc93945672941d5ff2b5f58d002979
aleydapacheco <aleydabelenpachecoherrera1234@gmail.com>
2024-08-08 10:02:46 -0400

    first commit

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+    declarations: [  
+        SelectorPageComponent,
+  ],
+    imports: [
+        CommonModule,
+        CountriesRoutingModule,
+        ReactiveFormsModule,
+    ],
+    exports: [
+        SelectorPageComponent,
+    ]
+})
+export class CountriesModule { }
